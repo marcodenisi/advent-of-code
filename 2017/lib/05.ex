2e65e87f58a4e23d852a3af5ebec39f37c6c32a6
Marco Denisi <marcodenisi35@gmail.com>
2020-12-15 14:49:13 +0100

    feat(2017.5): add 2017, day 5, part 2

--- 2017/lib/05.ex
+++ 2017/lib/05.ex
@@ -2,17 +2,26 @@ defmodule AOC.Day5 do
 
   def part_1(instructions) do
     instructions
-    |> move(0, 0)
+    |> move(0, 0, fn el -> el + 1 end)
   end
 
-  defp move(set, current_idx, count) when current_idx >= length(set), do: count
-  defp move(set, current_idx, count) do
-    steps = Enum.at(set, current_idx)
+  defp move(set, current_idx, count, _) when current_idx >= map_size(set), do: count
+  defp move(set, current_idx, count, change_fun) do
+    steps = Map.get(set, current_idx)
     move(
-      List.update_at(set, current_idx, fn el -> el + 1 end),
+      Map.put(set, current_idx, change_fun.(steps)),
       current_idx + steps,
-      count + 1
+      count + 1,
+      change_fun
     )
+  end
+
+  def part_2(instructions) do
+    instructions
+    |> move(0, 0, fn
+      el when el >= 3 -> el - 1
+      el -> el + 1
+    end)
   end
 
 end
--- 2017/test/05_test.exs
+++ 2017/test/05_test.exs
@@ -2,9 +2,11 @@ defmodule AOC.Day4Test do
   use ExUnit.Case
   doctest AOC.Day4
 
+  @tag timeout: :infinity
   test "Day 5 - real input" do
     {:ok, content} = File.read("resources/05.txt")
     assert AOC.Day5.part_1(parse(content)) == 339351
+    assert AOC.Day5.part_2(parse(content)) == 24315397
   end
 
   defp parse(content) do
@@ -12,6 +14,9 @@ defmodule AOC.Day4Test do
     |> String.split("\n")
     |> Enum.map(&String.trim/1)
     |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index
+    |> Enum.map(fn {el, idx} -> {idx, el} end)
+    |> Map.new
   end
 
 end
